d9fbfb838774e97c632f7a7662f228c8f3b417ce
Computer Maverick <43058711+ComputerMaverick69@users.noreply.github.com>
2019-08-30 13:05:09 -0700

    Virtual Environment created

---
+++ plugins/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+#
+#@name          :   CynalytIQ - Cyber AnalytIQ
+#@repository    :   https://github.com/reconnaissance-technologies/CynalytIQ
+#@author        :   Timmy Iwoni (Computer Maverick)
+#@license       :   See 'LICENSE.txt' file
